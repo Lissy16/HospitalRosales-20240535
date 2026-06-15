06b5aa03db0498fdf5eac7699a698177395fa367
Lissy16 <20240535@ricaldone.edu.sv>
2026-06-15 15:41:22 -0600

    cambiossssss profeeeeeeeeeeeeee

--- Desktop/Hospitallll/backend/app.js
+++ Desktop/Hospitallll/backend/app.js
@@ -8,14 +8,12 @@ app.use(express.json());
  
 app.use("/api/register", require("./src/routes/registerPatient"));
 app.use("/api/verify", require("./src/routes/verifyPatient"));
-app.use("/api/login", require("./src/routes/loginPatient"));
 app.use("/api/logout", require("./src/routes/logout"));
 app.use("/api/recovery", require("./src/routes/recoveryPassword"));
  
 app.use("/api/patients", require("./src/routes/patients"));
 app.use("/api/specialties", require("./src/routes/specialties"));
 app.use("/api/appointments", require("./src/routes/appointments"));
-app.use("/api/medical-records", require("./src/routes/medicalRecords"));
 app.use("/api/equipments", require("./src/routes/equipments"));
  
 app.use((err, req, res, next) => {
